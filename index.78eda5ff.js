d7d23d8bb0de1f1bcedd5f66d3c4346291f481a1
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-06-12 18:00:10 +0000

    Deploying to gh-pages from @ dmitkate/goteem-project@a0b8cf38544c3bbf405606a4e52e5bcc39308680 🚀

---
+++ index.78eda5ff.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open-products]"),closeModalBtn:document.querySelector("[data-modal-close-products]"),modal:document.querySelector("[data-modal-products]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.78eda5ff.js.map
