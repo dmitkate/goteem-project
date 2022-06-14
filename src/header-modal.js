87050885213d7432e8bf48e25bdba42a561f9e20
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-06-14 08:51:41 +0300

    bug-fixed
    cross btn - 30X30
    modal - body no-scroll
    div desktop menu - deleted

--- src/header-modal.js
+++ src/header-modal.js
@@ -4,13 +4,15 @@
      openModalBtne: document.querySelector("[data-header-modal-opene]"),
     closeModalBtn: document.querySelector("[data-header-modal-close]"),
     modal: document.querySelector("[data-header-modal]"),
+    body: document.querySelector('body')
   };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.openModalBtne.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
