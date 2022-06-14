f352897ca1c5289eabf17ba55188f383fb21d1ee
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-06-14 17:46:43 +0000

    Deploying to gh-pages from @ dmitkate/goteem-project@73f143a05e4ea166b197d952dcf24d3e9f0cf065 🚀

---
+++ index.6ffb1adf.js
@@ -0,0 +1,2 @@
+$((function(){$(".slider").slick({dots:!0,dotsClass:"my-dots",arrows:!1})}));
+//# sourceMappingURL=index.6ffb1adf.js.map
--- index.f9da52db.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".your-class").slick({dots:!0,dotsClass:"my-dots",arrows:!1})}));
-//# sourceMappingURL=index.f9da52db.js.map
