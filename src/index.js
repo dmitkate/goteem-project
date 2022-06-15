5bff2d114cfd49a37adaf0c389ac8e3a0f04491e
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-06-15 12:34:13 +0300

    фввrel="noreferrer noopener"

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './modal.js';
+import './modal-franchise.js';
+import './modal-location.js';
+import './mob-menu.js';
+import './header-modal.js';
+import './pageup.js';
+import './modal-products.js';
+import './modal-products-second.js';
+import './modal-products-third.js';
+import './pagination.js';
+import './modal-hero.js';
